7de8fa68ea8328c4cda893561a69c8d54201b87a
Max <maxime.leb@gmail.com>
2023-01-08 21:53:25 +0100

    contact

--- components/Contact.js
+++ components/Contact.js
@@ -26,8 +26,8 @@ function Contact() {
       <p>Après 7 années d'expérience dans la gestion des produits d'assurance et 4 années dans le design graphique,
         j'ai décidé de faire évoluer ma carrière vers le developpement web.
         Alumni du coding bootcamp de La Capsule Paris, je suis aujourd'hui développeur full-stack junior à Paris.
-        Sorti diplomé d'un titre RNCP de niveau 6 "Chef de projet d'application web & mobile", je poursuis mon apprentissage en étant, en paralèlle,
-        teacher assistant dans le bootcamp qui m'a formé. De ce fait, j'accompagne les éléves dans leurs problématiques liées au code
+        Sorti diplomé d'un titre RNCP de niveau 6 "Chef de projet d'application web & mobile", je poursuis mon apprentissage en étant, en parallèle,
+        teacher assistant dans le bootcamp qui m'a formé. De ce fait, j'accompagne les élèves dans leurs problématiques liées au code
         et je les aide aussi à se débugger.<br />
         Vous trouverez sur mon portfolio, mon stack ainsi que certains projets réalisés notamment en React & React-Native.<br />
         Je suis actuellement à la recherche d'un poste alors n'hésitez pas à me contacter pour que nous puissions échanger ensemble !</p>
