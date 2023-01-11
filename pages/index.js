030ca1c784fd5f305d110d2359ad9cd4643aaf08
Max <maxime.leb@gmail.com>
2023-01-11 19:38:33 +0100

    meta

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ function Index() {
     <>
       <Head>
         <title>Home / Maxime Leblanc Développeur Full Stack</title>
+        <meta name="google-site-verification" content="ZcjXUMk4jdl_EhVlHeojPdiZqltBwTJJFJpr8gCY7B0" />
         <meta name='description' content='Maxime Leblanc Développeur Portfolio' />
         <meta name='og:title' content='My Portfolio' />
         <link rel='icon' href='/favicon.ico' /> 
